syafnurr/press

commit 77f371073ccdae2c1b4014c240e43b05555d1750

refactor: Grouped site list in bench
- Use native ListView grouping feature
- replace custom API with standard list API
- simplify code

press/press/doctype/bench/bench.py
@@ -76,7 +76,32 @@ class Bench(Document):
 		vcpu: DF.Int
 	# end: auto-generated types
 
-	dashboard_fields = ["name", "group", "status"]
+	dashboard_fields = ["name", "group", "status", "is_ssh_proxy_setup", "proxy_server"]
+	dashboard_actions = ["restart", "rebuild", "update_all_sites"]
+
+	@staticmethod
+	def get_list_query(query):
+		Bench = frappe.qb.DocType("Bench")
+		benches = (
+			query.select(Bench.is_ssh_proxy_setup)
+			.where(Bench.status != "Archived")
+			.run(as_dict=1)
+		)
+		benches_with_patches = frappe.get_all(
+			"App Patch",
+			fields=["bench"],
+			filters={"bench": ["in", [d.name for d in benches]], "status": "Applied"},
+			pluck="bench",
+		)
+		for bench in benches:
+			bench.has_app_patch_applied = bench.name in benches_with_patches
+		return benches
+
+	def get_doc(self, doc):
+		user_ssh_key = frappe.db.get_all(
+			"User SSH Key", {"user": frappe.session.user, "is_default": 1}, limit=1
+		)
+		doc.user_ssh_key = bool(user_ssh_key)
 
 	@staticmethod
 	def with_sites(name: str):

press/press/doctype/release_group/release_group.py
@@ -745,15 +745,12 @@ class ReleaseGroup(Document, TagHelpers):
 
 	@frappe.whitelist()
 	def get_certificate(self):
-		user_ssh_key = (
-			frappe.get_all(
-				"User SSH Key", {"user": frappe.session.user, "is_default": True}, pluck="name"
-			)
-			or [None]
-		)[0]
+		user_ssh_key = frappe.db.get_all(
+			"User SSH Key", {"user": frappe.session.user, "is_default": True}, pluck="name"
+		)
 		if not user_ssh_key:
 			return False
-		certificates = frappe.get_all(
+		certificates = frappe.db.get_all(
 			"SSH Certificate",
 			{
 				"user": frappe.session.user,

dashboard/components.d.ts
@@ -110,6 +110,7 @@ declare module 'vue' {
     ILucideHelpCircle: typeof import('~icons/lucide/help-circle')['default']
     ILucideInfo: typeof import('~icons/lucide/info')['default']
     ILucideMenu: typeof import('~icons/lucide/menu')['default']
+    ILucideMoreHorizontal: typeof import('~icons/lucide/more-horizontal')['default']
     ILucidePlus: typeof import('~icons/lucide/plus')['default']
     ILucideRefreshCcw: typeof import('~icons/lucide/refresh-ccw')['default']
     ILucideSearch: typeof import('~icons/lucide/search')['default']
@@ -126,8 +127,12 @@ declare module 'vue' {
     JCB: typeof import('./src/components/icons/cards/JCB.vue')['default']
     LineChart: typeof import('./src/components/charts/LineChart.vue')['default']
     Link: typeof import('./src/components/Link.vue')['default']
+    ListEmptyState: typeof import('./node_modules/frappe-ui/src/components/ListView/ListEmptyState.vue')['default']
     ListFilter: typeof import('./node_modules/frappe-ui/src/components/ListFilter/ListFilter.vue')['default']
     ListFooter: typeof import('./node_modules/frappe-ui/src/components/ListView/ListFooter.vue')['default']
+    ListGroupHeader: typeof import('./node_modules/frappe-ui/src/components/ListView/ListGroupHeader.vue')['default']
+    ListGroupRows: typeof import('./node_modules/frappe-ui/src/components/ListView/ListGroupRows.vue')['default']
+    ListGroups: typeof import('./node_modules/frappe-ui/src/components/ListView/ListGroups.vue')['default']
     ListHeader: typeof import('./node_modules/frappe-ui/src/components/ListView/ListHeader.vue')['default']
     ListHeaderItem: typeof import('./node_modules/frappe-ui/src/components/ListView/ListHeaderItem.vue')['default']
     ListItem: typeof import('./src/components/global/ListItem.vue')['default']

dashboard/src2/components/ActionButton.vue
@@ -13,6 +13,7 @@
 </template>
 <script>
 import { Button, Dropdown } from 'frappe-ui';
+import { icon } from '../utils/components';
 
 export default {
 	name: 'ActionButton',
@@ -32,6 +33,13 @@ export default {
 				if (!this.$attrs.condition(this.$attrs.context)) return null;
 			}
 			return {
+				button: {
+					label: 'Options',
+					variant: 'ghost',
+					slots: {
+						icon: icon('more-horizontal')
+					}
+				},
 				...this.$attrs,
 				label: this.$attrs.label,
 				options: this.$attrs.options

dashboard/src2/components/ObjectList.vue
@@ -13,11 +13,7 @@
 					</template>
 					<template #suffix>
 						<span class="text-sm text-gray-500" v-if="searchQuery">
-							{{
-								filteredRows.length === 0
-									? 'No results'
-									: `${filteredRows.length} of ${rows.length}`
-							}}
+							{{ searchQuerySummary }}
 						</span>
 					</template>
 				</TextInput>
@@ -62,38 +58,17 @@
 						: () => {},
 					getRowRoute: this.options.route
 						? row => this.options.route(row)
-						: null
+						: null,
+					emptyState: {}
 				}"
 				row-key="name"
 			>
-				<ListHeader>
-					<ListHeaderItem
-						class="whitespace-nowrap"
-						v-for="column in columns"
-						:key="column.key"
-						:item="column"
-					>
-						<template #prefix>
-							<FeatherIcon
-								v-if="column.icon"
-								:name="column.icon"
-								class="h-4 w-4"
-							/>
-						</template>
-					</ListHeaderItem>
-				</ListHeader>
-				<ListRows>
-					<ListRow v-for="(row, i) in filteredRows" :row="row" :key="row.name">
-						<template v-slot="{ column, item }">
-							<ObjectListCell
-								:row="row"
-								:column="column"
-								:idx="i"
-								:context="context"
-							/>
-						</template>
-					</ListRow>
-				</ListRows>
+				<template v-if="options.groupHeader" #group-header="{ group }">
+					<component :is="options.groupHeader({ ...context, group })" />
+				</template>
+				<template #cell="{ item, row, column }">
+					<ObjectListCell :row="row" :column="column" :context="context" />
+				</template>
 			</ListView>
 			<div class="px-5" v-if="filteredRows.length === 0">
 				<div
@@ -279,21 +254,38 @@ export default {
 			if (!this.searchQuery) return this.rows;
 			let query = this.searchQuery.toLowerCase();
 
-			return this.rows.filter(row => {
-				let values = this.options.columns.map(column => {
-					let value = row[column.fieldname];
-					if (column.format) {
-						value = column.format(value, row);
-					}
-					return value;
-				});
-				for (let value of values) {
-					if (value && value.toLowerCase?.().includes(query)) {
-						return true;
-					}
+			return this.rows.map(row => {
+				if (row.rows) {
+					// group
+					return {
+						...row,
+						rows: row.rows.filter(row => this.filterRow(query, row))
+					};
+				}
+				if (this.filterRow(query, row)) {
+					return row;
 				}
 				return false;
 			});
+		},
+		searchQuerySummary() {
+			let result;
+			if (this.filteredRows.length === 0) {
+				result = 'No results';
+			} else if (this.filteredRows[0].rows) {
+				let total = this.rows.reduce(
+					(acc, group) => acc + group.rows.length,
+					0
+				);
+				let filtered = this.filteredRows.reduce(
+					(acc, group) => acc + group.rows.length,
+					0
+				);
+				result = `${filtered} of ${total}`;
+			} else {
+				result = `${this.filteredRows.length} of ${rows.length}`;
+			}
+			return result;
 		},
 		primaryAction() {
 			if (!this.options.primaryAction) return null;
@@ -319,6 +311,23 @@ export default {
 		},
 		hideControls() {
 			return !this.options.hideControls;
+		}
+	},
+	methods: {
+		filterRow(query, row) {
+			let values = this.options.columns.map(column => {
+				let value = row[column.fieldname];
+				if (column.format) {
+					value = column.format(value, row);
+				}
+				return value;
+			});
+			for (let value of values) {
+				if (value && value.toLowerCase?.().includes(query)) {
+					return true;
+				}
+			}
+			return false;
 		}
 	}
 };

dashboard/src2/components/bench/SSHCertificateDialog.vue
@@ -6,29 +6,8 @@
 		}"
 		v-model="show"
 	>
-		<template #body-content>
-			<div v-if="!bench.hasSSHAcess" class="space-y-4">
-				<p class="mb-4 text-base">
-					It looks like you haven't added your SSH public key.<br />
-					Please go to
-					<router-link
-						:to="{ name: 'SettingsDeveloper' }"
-						class="underline"
-						@click="show = false"
-					>
-						Developer Settings
-					</router-link>
-					and add your SSH public key.
-				</p>
-				<p class="text-base">
-					Please refer to the
-					<a href="/docs/benches/ssh" class="underline" target="_blank"
-						>SSH Access documentation</a
-					>
-					for more details.
-				</p>
-			</div>
-			<div v-else-if="certificate" class="space-y-4">
+		<template #body-content v-if="$bench.doc">
+			<div v-if="certificate" class="space-y-4">
 				<div class="space-y-2">
 					<h4 class="text-base font-semibold text-gray-700">Step 1</h4>
 					<div class="space-y-1">
@@ -39,7 +18,6 @@
 						<ClickToCopyField :textContent="certificateCommand" />
 					</div>
 				</div>
-
 				<div class="space-y-2">
 					<h4 class="text-base font-semibold text-gray-700">Step 2</h4>
 					<div class="space-y-1">
@@ -50,22 +28,44 @@
 					</div>
 				</div>
 			</div>
-			<div v-else>
-				<p class="mb-4 text-base">
+			<div class="space-y-2 text-p-base text-gray-700" v-else>
+				<p v-if="!$bench.doc.user_ssh_key">
+					It looks like you haven't added your SSH public key. Go to
+					<router-link
+						:to="{ name: 'SettingsDeveloper' }"
+						class="underline"
+						@click="show = false"
+					>
+						Developer Settings</router-link
+					>
+					to add your SSH public key.
+				</p>
+				<p v-else-if="!$bench.doc.is_ssh_proxy_setup">
+					SSH access is not enabled for this bench. Please contact support to
+					enable access.
+				</p>
+				<p v-else>
 					You will need an SSH certificate to get SSH access to your bench. This
 					certificate will work only with your public-private key pair and will
 					be valid for 6 hours.
 				</p>
-				<p class="text-base">
+				<p>
 					Please refer to the
-					<a href="/docs/benches/ssh" class="underline"
+					<a href="/docs/benches/ssh" class="underline" target="_blank"
 						>SSH Access documentation</a
 					>
 					for more details.
 				</p>
 			</div>
 		</template>
-		<template #actions v-if="!certificate && bench.hasSSHAcess">
+		<template
+			#actions
+			v-if="
+				!certificate &&
+				$bench.doc?.is_ssh_proxy_setup &&
+				$bench.doc?.user_ssh_key
+			"
+		>
 			<Button
 				:loading="$releaseGroup.generateCertificate.loading"
 				@click="
@@ -88,7 +88,6 @@
 
 <script>
 import { getCachedDocumentResource } from 'frappe-ui';
-import bench from '../../objects/bench';
 
 export default {
 	props: ['bench', 'releaseGroup'],
@@ -97,10 +96,24 @@ export default {
 			show: true
 		};
 	},
-	mounted() {
-		this.$releaseGroup.getCertificate.submit();
+	resources: {
+		bench() {
+			return {
+				type: 'document',
+				doctype: 'Bench',
+				name: this.bench,
+				onSuccess(doc) {
+					if (doc.is_ssh_proxy_setup && doc.user_ssh_key) {
+						this.$releaseGroup.getCertificate.reload();
+					}
+				}
+			};
+		}
 	},
 	computed: {
+		$bench() {
+			return this.$resources.bench;
+		},
 		$releaseGroup() {
 			return getCachedDocumentResource('Release Group', this.releaseGroup);
 		},
@@ -108,7 +121,8 @@ export default {
 			return this.$releaseGroup.getCertificate.data;
 		},
 		sshCommand() {
-			return `ssh ${this.bench?.name}@${this.bench?.proxyServer} -p 2222`;
+			if (!this.$bench.doc) return;
+			return `ssh ${this.$bench.doc.name}@${this.$bench.doc.proxy_server} -p 2222`;
 		},
 		certificateCommand() {
 			if (this.certificate) {

dashboard/src2/objects/bench.js
@@ -486,18 +486,18 @@ export default {
 						{
 							label: 'Site',
 							fieldname: 'site',
-							class: 'text-gray-600'
+							class: 'text-gray-700'
 						},
 						{
 							label: 'Job ID',
 							fieldname: 'job_id',
-							class: 'text-gray-600',
+							class: 'text-gray-700',
 							width: '7rem'
 						},
 						{
 							label: 'Duration',
 							fieldname: 'duration',
-							class: 'text-gray-600',
+							class: 'text-gray-700',
 							width: '4rem',
 							format(value, row) {
 								if (row.job_id === 0 || !row.end) return;
@@ -507,7 +507,8 @@ export default {
 						{
 							label: 'Created By',
 							fieldname: 'owner',
-							width: '10rem'
+							width: '10rem',
+							class: 'text-gray-700'
 						},
 						{
 							label: '',
@@ -925,12 +926,6 @@ export default {
 				},
 				{
 					label: 'Options',
-					button: {
-						label: 'Options',
-						slots: {
-							icon: icon('more-horizontal')
-						}
-					},
 					options: [
 						{
 							label: 'View in Desk',

dashboard/src2/objects/site.js
@@ -12,7 +12,7 @@ import ObjectList from '../components/ObjectList.vue';
 import { getTeam, switchToTeam } from '../data/team';
 import router from '../router';
 import { confirmDialog, icon, renderDialog } from '../utils/components';
-import { bytes, duration, date, plural } from '../utils/format';
+import { bytes, duration, date, userCurrency } from '../utils/format';
 import { getRunningJobs } from '../utils/agentJob';
 import SiteActions from '../components/SiteActions.vue';
 import { tagTab } from './common/tags';
@@ -85,10 +85,11 @@ export default {
 					let $team = getTeam();
 					if (row.price_usd > 0) {
 						let india = $team.doc.country == 'India';
-						let currencySymbol = $team.doc.currency == 'INR' ? '₹' : '$';
-						return `${currencySymbol}${
-							india ? row.price_inr : row.price_usd
-						} /mo`;
+						let formattedValue = userCurrency(
+							india ? row.price_inr : row.price_usd,
+							0
+						);
+						return `${formattedValue} /mo`;
 					}
 					return row.plan_title;
 				}
@@ -1227,12 +1228,6 @@ export default {
 				},
 				{
 					label: 'Options',
-					button: {
-						label: 'Options',
-						slots: {
-							icon: icon('more-horizontal')
-						}
-					},
 					context,
 					options: [
 						{

commit 7518af62c6c8b4347b303605103f32d29b0ec87e

fix(release-group): ssh certificate and proxy_server field fetching
regression from 77f371073ccdae2c1b4014c240e43b05555d1750

press/press/doctype/bench/bench.py
@@ -76,7 +76,7 @@ class Bench(Document):
 		vcpu: DF.Int
 	# end: auto-generated types
 
-	dashboard_fields = ["name", "group", "status", "is_ssh_proxy_setup", "proxy_server"]
+	dashboard_fields = ["name", "group", "status", "is_ssh_proxy_setup"]
 	dashboard_actions = ["restart", "rebuild", "update_all_sites"]
 
 	@staticmethod
@@ -102,6 +102,7 @@ class Bench(Document):
 			"User SSH Key", {"user": frappe.session.user, "is_default": 1}, limit=1
 		)
 		doc.user_ssh_key = bool(user_ssh_key)
+		doc.proxy_server = frappe.db.get_value("Server", self.server, "proxy_server")
 
 	@staticmethod
 	def with_sites(name: str):

press/press/doctype/release_group/release_group.py
@@ -748,7 +748,7 @@ class ReleaseGroup(Document, TagHelpers):
 		user_ssh_key = frappe.db.get_all(
 			"User SSH Key", {"user": frappe.session.user, "is_default": True}, pluck="name"
 		)
-		if not user_ssh_key:
+		if not len(user_ssh_key):
 			return False
 		certificates = frappe.db.get_all(
 			"SSH Certificate",
@@ -756,7 +756,7 @@ class ReleaseGroup(Document, TagHelpers):
 				"user": frappe.session.user,
 				"valid_until": [">", frappe.utils.now()],
 				"group": self.name,
-				"user_ssh_key": user_ssh_key,
+				"user_ssh_key": user_ssh_key[0],
 			},
 			pluck="name",
 			limit=1,