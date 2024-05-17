syafnurr/press

commit f8cb83c2afd1d86641737a14d87340407ef64eae

refactor: simpler permission check

press/press/doctype/press_role/press_role.py
@@ -82,44 +82,46 @@ def check_role_permissions(doctype: str, name: str | None = None) -> list[str] |
 	:param name: Document name
 	:return: List of permitted roles or None
 	"""
+	if doctype not in ["Site", "Release Group", "Server", "Marketplace App"]:
+		return []
+
 	if frappe.local.system_user():
 		return []
 
-	PressRoleUser = frappe.qb.DocType("Press Role User")
+	PressRolePermission = frappe.qb.DocType("Press Role Permission")
 	PressRole = frappe.qb.DocType("Press Role")
 
-	roles = (
+	query = (
 		frappe.qb.from_(PressRole)
 		.select(PressRole.name)
-		.join(PressRoleUser)
-		.on(PressRoleUser.parent == PressRole.name)
-		.where(PressRoleUser.user == frappe.session.user)
 		.where(PressRole.team == frappe.local.team().name)
-	).run(as_dict=1, pluck="name")
+	)
+
+	if doctype == "Marketplace App":
+		query = query.select(PressRole.enable_apps)
+	else:
+		field = doctype.lower().replace(" ", "_")
+		query = (
+			query.select(PressRolePermission[field])
+			.join(PressRolePermission)
+			.on(PressRolePermission.role == PressRole.name)
+		)
+
+	roles = query.run(as_dict=1)
+
+	if not roles:
+		return []
 
 	if doctype in ["Site", "Release Group", "Server"]:
-		field = doctype.lower().replace(" ", "_")
-		if role_perms := frappe.get_all(
-			"Press Role Permission",
-			fields=["name", field],
-			filters={"role": ("in", roles)},
-		):
-			# throw error if the user is not permitted for the document
-			if name and not any(perm[field] == name for perm in role_perms):
-				frappe.throw("Not permitted", frappe.PermissionError)
-			else:
-				return roles
+		# throw error if the user is not permitted for the document
+		if name and not any(perm[field] == name for perm in roles):
+			frappe.throw("Not permitted", frappe.PermissionError)
+		else:
+			return [perm["name"] for perm in roles]
 
 	# throw error if any of the roles don't have permission for apps
 	elif doctype == "Marketplace App":
-		if not any(
-			frappe.get_all(
-				"Press Role",
-				fields=["enable_apps"],
-				filters={"name": ("in", roles)},
-				pluck="enable_apps",
-			)
-		):
+		if not any(perm.enable_apps for perm in roles):
 			frappe.throw("Not permitted", frappe.PermissionError)
 
 	return []

commit 7e1139971a7bcf7235416cf6367d570fa473e0ec

fix(permission-role): join with user
regression from f8cb83c2afd1d86641737a14d87340407ef64eae

press/press/doctype/press_role/press_role.py
@@ -89,11 +89,15 @@ def check_role_permissions(doctype: str, name: str | None = None) -> list[str] |
 		return []
 
 	PressRolePermission = frappe.qb.DocType("Press Role Permission")
+	PressRoleUser = frappe.qb.DocType("Press Role User")
 	PressRole = frappe.qb.DocType("Press Role")
 
 	query = (
 		frappe.qb.from_(PressRole)
 		.select(PressRole.name)
+		.join(PressRoleUser)
+		.on(PressRoleUser.parent == PressRole.name)
+		.where(PressRoleUser.user == frappe.session.user)
 		.where(PressRole.team == frappe.local.team().name)
 	)
 