syafnurr/press

commit 3f04819468dbd01bac3ced201cd4bfe7fc7dda42

refactor: Let user add payment info on upgrading plan for trial site (#2493)
* refactor(signup): let user add payment info on trying to upgrade trial site

* fix(signup): explicitly convey for which account the setup is for

* fix: add a banner asking user to upgrade

* fix: smaller plan dialog

* fix: minor style changes to the check symbol info

* fix: don't add last name if null

* refactor(upsell-banner): make the upsell banner gray

* refactor(first-payment): explain payment modes

- prefill user name in billing details if possible

* refactor(first-payment): add progress bar

dashboard/src2/components/PlansCards.vue
@@ -10,31 +10,26 @@
 						? 'border-gray-900 ring-1 ring-gray-900'
 						: 'border-gray-300',
 					{
-						'pointer-events-none opacity-50': plan.disabled
-					}
+						'pointer-events-none opacity-50': plan.disabled,
+					},
 				]"
 				@click="$emit('update:modelValue', plan)"
 			>
 				<div
-					class="w-full border-b p-3"
+					class="h-16 w-full border-b p-3"
 					:class="[
-						modelValue === plan ? 'border-gray-900 ring-1 ring-gray-900' : ''
+						modelValue === plan ? 'border-gray-900 ring-1 ring-gray-900' : '',
 					]"
 				>
 					<div class="flex items-center justify-between">
-						<div class="flex items-center text-lg">
-							<span class="font-medium text-gray-900">
-								<template v-if="plan.label">
-									{{ plan.label }}
-								</template>
-								<template v-else>
-									{{ $format.planTitle(plan)
-									}}<span v-if="plan.price_inr" class="text-gray-700">/mo</span>
-								</template>
+						<div class="flex w-full items-center text-lg">
+							<span class="truncate font-medium text-gray-900">
+								{{ $format.planTitle(plan)
+								}}<span v-if="plan.price_inr" class="text-gray-700">/mo</span>
 							</span>
 							<Tooltip text="Support included">
 								<i-lucide-badge-check
-									class="ml-1 h-4 w-4 text-gray-600"
+									class="ml-1 h-4 w-6 text-gray-600"
 									v-if="plan.support_included"
 								/>
 							</Tooltip>
@@ -44,14 +39,14 @@
 						<template v-if="plan.sublabel">
 							{{ plan.sublabel }}
 						</template>
-						<template v-else>
+						<template v-else-if="plan.price_inr || plan.price_usd">
 							{{
 								$format.userCurrency(
 									$format.pricePerDay(
 										$team.doc.currency === 'INR'
 											? plan.price_inr
-											: plan.price_usd
-									)
+											: plan.price_usd,
+									),
 								)
 							}}/day
 						</template>
@@ -87,7 +82,7 @@ export default {
 	methods: {
 		_icon(iconName, classes) {
 			return icon(iconName, classes);
-		}
-	}
+		},
+	},
 };
 </script>

dashboard/src2/components/DismissableBanner.vue
@@ -17,16 +17,15 @@ export default {
 		title: String,
 		id: {
 			type: String,
-			required: true
+			required: true,
 		},
 		type: {
 			type: String,
-			default: 'info'
-		}
+		},
 	},
 	data() {
 		return {
-			_show: true
+			_show: true,
 		};
 	},
 	components: { AlertBanner },
@@ -36,7 +35,7 @@ export default {
 				if (!this._show) return false;
 
 				const dismissedDate = parseInt(
-					localStorage.getItem(`dismissed-banner-${this.id}`)
+					localStorage.getItem(`dismissed-banner-${this.id}`),
 				);
 				if (!dismissedDate) return true;
 
@@ -50,14 +49,14 @@ export default {
 			},
 			set(value) {
 				this._show = value;
-			}
-		}
+			},
+		},
 	},
 	methods: {
 		dismiss() {
 			this.show = false;
 			localStorage.setItem(`dismissed-banner-${this.id}`, Date.now());
-		}
-	}
+		},
+	},
 };
 </script>

dashboard/src2/components/ManageSitePlansDialog.vue
@@ -1,59 +1,179 @@
 <template>
 	<Dialog
 		:options="{
-			title: 'Change Plan',
-			size: '5xl',
-			actions: [
-				{
-					label: 'Change plan',
-					variant: 'solid',
-					onClick: changePlan,
-					disabled: !plan || ($site?.doc && plan === $site.doc.plan)
-				}
-			]
+			title: showSetupSubscription ? 'Setup Subscription' : 'Change Plan',
+			size: step === 'site-plans' ? '3xl' : '3xl',
 		}"
 		v-model="show"
 	>
 		<template #body-content>
-			<SitePlansCards
-				v-model="plan"
-				:isPrivateBenchSite="!$site.doc.group_public"
-				:isDedicatedServerSite="$site.doc.is_dedicated_server"
-			/>
-			<div class="mt-4 text-xs text-gray-700">
-				<div
-					class="flex items-center rounded bg-gray-50 p-2 text-p-base font-medium text-gray-800"
-				>
-					<i-lucide-badge-check class="h-4 w-8 text-gray-600" />
-					<span>
-						<strong>Support</strong> covers only issues of Frappe apps and not
-						functional queries. You can raise a support ticket for Frappe Cloud
-						issues for all plans.
-					</span>
+			<!-- steps are for users without payment method added, 
+		 otherwise user will only go through just the initial step to change plan  -->
+
+			<div v-if="step === 'site-plans'">
+				<!-- doing this weird thing because progress with intervals doesn't rerender on moving to new step for some reason -->
+				<!-- TODO: fix it in frappe-ui -->
+				<Progress
+					v-if="showSetupSubscription"
+					class="my-8"
+					size="md"
+					:label="progressLabel"
+					:interval-count="3"
+					:intervals="true"
+					:value="32"
+				/>
+				<SitePlansCards
+					v-model="plan"
+					:isPrivateBenchSite="!$site.doc.group_public"
+					:isDedicatedServerSite="$site.doc.is_dedicated_server"
+				/>
+				<div class="mt-4 text-xs text-gray-700">
+					<div
+						class="flex items-center rounded bg-gray-50 p-2 text-p-base font-medium text-gray-800"
+					>
+						<i-lucide-badge-check class="mr-2 h-5 w-12 text-gray-600" />
+						<span>
+							<strong>Support</strong> covers only issues of Frappe apps and not
+							functional queries. You can raise a support ticket for Frappe
+							Cloud issues for all plans.
+						</span>
+					</div>
 				</div>
+				<ErrorMessage class="mt-2" :message="$site.setPlan.error" />
 			</div>
-			<ErrorMessage class="mt-2" :message="$site.setPlan.error" />
+
+			<div v-else-if="step === 'billing-details'">
+				<Progress
+					class="my-8"
+					size="md"
+					:label="progressLabel"
+					:interval-count="3"
+					:intervals="true"
+					:value="65"
+				/>
+				<div class="mb-5 inline-flex gap-1.5 text-base text-gray-700">
+					<FeatherIcon class="h-4" name="info" />
+					<span> Add billing details to your account before proceeding.</span>
+				</div>
+				<BillingDetails ref="billingRef" @success="step = 'add-payment-mode'" />
+			</div>
+
+			<div v-else-if="step === 'add-payment-mode'">
+				<Progress
+					class="my-8"
+					:label="progressLabel"
+					size="md"
+					:interval-count="3"
+					:intervals="true"
+					:value="99"
+				/>
+				<div
+					class="mb-5 flex w-full flex-row gap-2 rounded-md border p-1 text-p-base text-gray-800"
+				>
+					<div
+						class="w-1/2 cursor-pointer rounded-[7px] py-1.5 text-center transition-all"
+						:class="{
+							'bg-gray-100': isAutomatedBilling,
+						}"
+						@click="isAutomatedBilling = true"
+					>
+						Add Card
+					</div>
+					<div
+						class="w-1/2 cursor-pointer rounded-sm py-1.5 text-center transition-all"
+						:class="{
+							'bg-gray-100': !isAutomatedBilling,
+						}"
+						@click="isAutomatedBilling = false"
+					>
+						Add Prepaid Credits
+					</div>
+				</div>
+
+				<div>
+					<div
+						v-if="isAutomatedBilling"
+						class="mb-5 flex items-center gap-2 text-sm text-gray-700"
+					>
+						<FeatherIcon class="h-4" name="info" />
+						<span>
+							Adding a card will enable automated billing for your account. You
+							will be charged automatically at the end of your billing cycle.
+						</span>
+					</div>
+					<div
+						v-else
+						class="mb-5 flex items-center gap-2 text-sm text-gray-700"
+					>
+						<FeatherIcon class="h-4" name="info" />
+						<span>
+							Adding prepaid credits will allow you to manually recharge your
+							account balance. You can use this balance to pay for your plan.
+						</span>
+					</div>
+				</div>
+
+				<CardForm v-if="isAutomatedBilling" @success="paymentModeAdded" />
+				<PrepaidCreditsForm
+					v-else
+					:minimumAmount="
+						$team.doc?.currency === 'INR' ? plan.price_inr : plan.price_usd
+					"
+					@success="paymentModeAdded"
+				/>
+			</div>
+		</template>
+		<template #actions v-if="step === 'site-plans'">
+			<Button
+				variant="solid"
+				:disabled="!plan || ($site?.doc && plan === $site.doc.plan)"
+				@click="handleNext()"
+				class="w-full"
+			>
+				{{
+					!$team.doc.payment_mode ||
+					!$team.doc.billing_details ||
+					!Object.keys(this.$team.doc.billing_details).length
+						? 'Next'
+						: $site.doc?.current_plan?.is_trial_plan
+							? 'Upgrade Plan'
+							: 'Change plan'
+				}}
+			</Button>
 		</template>
 	</Dialog>
 </template>
 <script>
-import { getCachedDocumentResource } from 'frappe-ui';
+import { getCachedDocumentResource, Progress } from 'frappe-ui';
 import SitePlansCards from './SitePlansCards.vue';
 import { getPlans, getPlan } from '../data/plans';
+import CardForm from './billing/CardForm.vue';
+import BillingDetails from './billing/BillingDetails.vue';
+import PrepaidCreditsForm from './billing/PrepaidCreditsForm.vue';
 
 export default {
 	name: 'ManageSitePlansDialog',
-	components: { SitePlansCards },
+	components: {
+		CardForm,
+		Progress,
+		SitePlansCards,
+		BillingDetails,
+		PrepaidCreditsForm,
+	},
 	props: {
 		site: {
 			type: String,
-			required: true
-		}
+			required: true,
+		},
 	},
 	data() {
 		return {
 			show: true,
-			plan: null
+			plan: null,
+			step: 'site-plans',
+			isAutomatedBilling: true,
+			showAddPaymentModeDialog: false,
+			showBillingDetailsDialog: false,
 		};
 	},
 	watch: {
@@ -65,10 +185,23 @@ export default {
 						this.plan = getPlan(this.$site.doc.plan);
 					}
 				}
-			}
-		}
+			},
+		},
 	},
 	methods: {
+		handleNext() {
+			if (
+				!this.$team.doc.billing_details ||
+				!Object.keys(this.$team.doc.billing_details).length
+			) {
+				this.step = 'billing-details';
+				this.$team.reload();
+			} else if (!this.$team.doc.payment_mode) {
+				this.step = 'add-payment-mode';
+			} else {
+				this.changePlan();
+			}
+		},
 		changePlan() {
 			return this.$site.setPlan.submit(
 				{ plan: this.plan.name },
@@ -76,21 +209,45 @@ export default {
 					onSuccess: () => {
 						this.show = false;
 						let plan = getPlans().find(
-							plan => plan.name === this.$site.doc.plan
+							(plan) => plan.name === this.$site.doc.plan,
 						);
 						let formattedPlan = plan
 							? `${this.$format.planTitle(plan)}/mo`
 							: this.$site.doc.plan;
 						this.$toast.success(`Plan changed to ${formattedPlan}`);
-					}
-				}
+					},
+				},
 			);
-		}
+		},
+		paymentModeAdded() {
+			this.$team.reload();
+			this.show = false;
+			this.$toast.success(
+				'Payment mode added and the plan has been changed successfully',
+			);
+			this.changePlan();
+		},
 	},
 	computed: {
 		$site() {
 			return getCachedDocumentResource('Site', this.site);
-		}
-	}
+		},
+		showSetupSubscription() {
+			return (
+				!this.$team.doc.payment_mode ||
+				!this.$team.doc.billing_details ||
+				!Object.keys(this.$team.doc.billing_details).length
+			);
+		},
+		progressLabel() {
+			if (this.step === 'site-plans') {
+				return 'Select a plan';
+			} else if (this.step === 'billing-details') {
+				return 'Add billing details';
+			} else if (this.step === 'add-payment-mode') {
+				return 'Add payment mode';
+			}
+		},
+	},
 };
 </script>

dashboard/src2/components/SiteOverview.vue
@@ -18,12 +18,17 @@
 				Login
 			</Button>
 		</AlertBanner>
-		<AlertAddPaymentMode
-			v-if="!$team.doc.payment_mode"
+
+		<AlertBanner
+			v-if="$site.doc.current_plan?.is_trial_plan"
 			class="col-span-1 lg:col-span-2"
-			title="Add a payment mode to upgrade your plan"
-			type="info"
-		/>
+			title="Upgrade to a paid plan to continue using your site after the trial period."
+		>
+			<Button class="ml-auto" variant="outline" @click="showPlanChangeDialog">
+				Upgrade
+			</Button>
+		</AlertBanner>
+
 		<DismissableBanner
 			v-if="$site.doc.eol_versions.includes($site.doc.version)"
 			class="col-span-1 lg:col-span-2"
@@ -48,6 +53,7 @@
 			class="col-span-1 lg:col-span-2"
 			title="Your site is currently on a shared bench group. Upgrade plan to enjoy <a href='https://frappecloud.com/shared-hosting#benches' class='underline' target='_blank'>more benefits</a>."
 			:id="$site.name"
+			type="gray"
 		>
 			<Button class="ml-auto" variant="outline" @click="showPlanChangeDialog">
 				Upgrade Plan
@@ -89,10 +95,7 @@
 								</div>
 							</div>
 						</div>
-						<Button
-							@click="showPlanChangeDialog"
-							:disabled="!$team.doc.payment_mode"
-						>
+						<Button @click="showPlanChangeDialog">
 							{{ currentPlan?.is_trial_plan ? 'Upgrade' : 'Change' }}
 						</Button>
 					</div>
@@ -275,7 +278,6 @@ import { renderDialog } from '../utils/components';
 import SiteDailyUsage from './SiteDailyUsage.vue';
 import AlertBanner from './AlertBanner.vue';
 import { trialDays } from '../utils/site';
-import AlertAddPaymentMode from './AlertAddPaymentMode.vue';
 
 export default {
 	name: 'SiteOverview',
@@ -285,7 +287,6 @@ export default {
 		Progress,
 		AlertBanner,
 		DismissableBanner,
-		AlertAddPaymentMode,
 	},
 	data() {
 		return {

dashboard/src2/components/billing/BillingDetails.vue
@@ -27,14 +27,20 @@
 <script setup>
 import NewAddressForm from './NewAddressForm.vue';
 import { FormControl, ErrorMessage, Button, createResource } from 'frappe-ui';
-import { reactive, ref } from 'vue';
+import { reactive, ref, inject } from 'vue';
 
 const emit = defineEmits(['success']);
 
+const team = inject('team');
+
 const addressFormRef = ref(null);
 
+const fullName = `${team.doc.user_info.first_name || ''} ${
+	team.doc.user_info.last_name || ''
+}`.trim();
+
 const billingInformation = reactive({
-	billing_name: '',
+	billing_name: fullName || '',
 	address: '',
 	city: '',
 	state: '',
@@ -47,6 +53,8 @@ createResource({
 	url: 'press.api.account.get_billing_information',
 	auto: true,
 	onSuccess: (data) => {
+		if (!Object.keys(data).length) return;
+
 		Object.assign(billingInformation, {
 			address: data.address_line1,
 			city: data.city,

dashboard/src2/components/billing/CardForm.vue
@@ -45,16 +45,6 @@
 					card works. This amount will be <strong>refunded</strong> back to your
 					account.
 				</p>
-
-				<Button
-					:loading="!microChargeCompleted"
-					:loadingText="'Verifying Card'"
-				>
-					Card Verified
-					<template #prefix>
-						<GreenCheckIcon class="h-4 w-4" />
-					</template>
-				</Button>
 			</div>
 
 			<ErrorMessage class="mt-2" :message="errorMessage" />
@@ -77,22 +67,31 @@
 					:loading="addingCard"
 					@click="submit"
 				/>
+
+				<Button
+					v-else-if="tryingMicroCharge"
+					:loading="!microChargeCompleted"
+					:loadingText="'Verifying Card'"
+				>
+					Card Verified
+					<template #prefix>
+						<GreenCheckIcon class="h-4 w-4" />
+					</template>
+				</Button>
 			</div>
 		</div>
 	</div>
 </template>
 <script setup>
-import AddressForm from '../AddressForm.vue';
 import NewAddressForm from './NewAddressForm.vue';
 import PoweredByStripeLogo from '../../logo/PoweredByStripeLogo.vue';
-// import GreenCheckIcon from '../icons/GreenCheckIcon.vue';
 import {
 	FeatherIcon,
 	Button,
 	FormControl,
 	Spinner,
 	ErrorMessage,
-	createResource
+	createResource,
 } from 'frappe-ui';
 import { currency } from '../../utils/format';
 import { loadStripe } from '@stripe/stripe-js';
@@ -121,7 +120,7 @@ const cardElementRef = ref(null);
 
 const getPublishedKeyAndSetupIntent = createResource({
 	url: 'press.api.billing.get_publishable_key_and_setup_intent',
-	onSuccess: async data => {
+	onSuccess: async (data) => {
 		const { publishable_key, setup_intent } = data;
 		_setupIntent.value = setup_intent;
 		stripe.value = await loadStripe(publishable_key);
@@ -143,41 +142,41 @@ const getPublishedKeyAndSetupIntent = createResource({
 					'"Apple Color Emoji"',
 					'"Segoe UI Emoji"',
 					'"Segoe UI Symbol"',
-					'"Noto Color Emoji"'
+					'"Noto Color Emoji"',
 				].join(', '),
 				fontSmoothing: 'antialiased',
 				fontSize: '13px',
 				'::placeholder': {
-					color: '#C7C7C7'
-				}
+					color: '#C7C7C7',
+				},
 			},
 			invalid: {
 				color: '#C7C7C7',
-				iconColor: '#C7C7C7'
-			}
+				iconColor: '#C7C7C7',
+			},
 		};
 		card.value = elements.value.create('card', {
 			hidePostalCode: true,
 			style: style,
 			classes: {
 				complete: '',
-				focus: 'bg-gray-100'
-			}
+				focus: 'bg-gray-100',
+			},
 		});
 		card.value.mount(cardElementRef.value);
-		card.value.addEventListener('change', event => {
+		card.value.addEventListener('change', (event) => {
 			cardErrorMessage.value = event.error?.message || null;
 		});
 		card.value.addEventListener('ready', () => {
 			ready.value = true;
 		});
-	}
+	},
 });
 
 const countryList = createResource({
 	url: 'press.api.account.country_list',
 	cache: 'countryList',
-	auto: true
+	auto: true,
 });
 
 const browserTimezone = computed(() => {
@@ -190,14 +189,14 @@ const browserTimezone = computed(() => {
 const billingInformation = reactive({
 	cardHolderName: '',
 	country: '',
-	gstin: ''
+	gstin: '',
 });
 
 createResource({
 	url: 'press.api.account.get_billing_information',
 	params: { timezone: browserTimezone.value },
 	auto: true,
-	onSuccess: data => {
+	onSuccess: (data) => {
 		billingInformation.country = data?.country;
 		billingInformation.address = data?.address_line1;
 		billingInformation.city = data?.city;
@@ -205,7 +204,7 @@ createResource({
 		billingInformation.postal_code = data?.pincode;
 		billingInformation.gstin =
 			data?.gstin == 'Not Applicable' ? '' : data?.gstin;
-	}
+	},
 });
 
 const setupIntentSuccess = createResource({
@@ -213,7 +212,7 @@ const setupIntentSuccess = createResource({
 	makeParams: ({ setupIntent }) => {
 		return {
 			setup_intent: setupIntent,
-			address: billingInformation
+			address: billingInformation,
 		};
 	},
 	onSuccess: async ({ payment_method_name }) => {
@@ -221,12 +220,12 @@ const setupIntentSuccess = createResource({
 		addingCard.value = false;
 		toast.success('Card added successfully');
 	},
-	onError: error => {
+	onError: (error) => {
 		console.error(error);
 		addingCard.value = false;
 		errorMessage.value = error.messages.join('\n');
 		toast.error(errorMessage.value);
-	}
+	},
 });
 
 const verifyCardWithMicroCharge = createResource({
@@ -234,11 +233,11 @@ const verifyCardWithMicroCharge = createResource({
 	makeParams: ({ paymentMethodName }) => {
 		return { payment_method_name: paymentMethodName };
 	},
-	onSuccess: async paymentIntent => {
+	onSuccess: async (paymentIntent) => {
 		let { client_secret } = paymentIntent;
 
 		let payload = await stripe.value.confirmCardPayment(client_secret, {
-			payment_method: { card: card.value }
+			payment_method: { card: card.value },
 		});
 
 		if (payload.paymentIntent?.status === 'succeeded') {
@@ -246,17 +245,17 @@ const verifyCardWithMicroCharge = createResource({
 			emit('success');
 		}
 	},
-	onError: error => {
+	onError: (error) => {
 		console.error(error);
 		tryingMicroCharge.value = false;
 		errorMessage.value = error.messages.join('\n');
-	}
+	},
 });
 
 async function setupStripeIntent() {
 	await getPublishedKeyAndSetupIntent.submit();
 	const { first_name, last_name = '' } = team.doc?.user_info;
-	const fullname = first_name + ' ' + last_name;
+	const fullname = `${first_name} ${last_name ?? ''}`;
 	billingInformation.cardHolderName = fullname.trimEnd();
 }
 
@@ -286,11 +285,11 @@ async function submit() {
 						city: billingInformation.city,
 						state: billingInformation.state,
 						postal_code: billingInformation.postal_code,
-						country: getCountryCode(team.doc?.country)
-					}
-				}
-			}
-		}
+						country: getCountryCode(team.doc?.country),
+					},
+				},
+			},
+		},
 	);
 	if (error) {
 		addingCard.value = false;
@@ -331,12 +330,12 @@ async function verifyWithMicroChargeIfApplicable(paymentMethodName) {
 async function _verifyWithMicroCharge(paymentMethodName) {
 	tryingMicroCharge.value = true;
 	return verifyCardWithMicroCharge.submit({
-		paymentMethodName
+		paymentMethodName,
 	});
 }
 
 function getCountryCode(country) {
-	let code = countryList.data.find(d => d.name === country).code;
+	let code = countryList.data.find((d) => d.name === country).code;
 	return code.toUpperCase();
 }
 
@@ -354,7 +353,7 @@ const formattedMicroChargeAmount = computed(() => {
 	}
 	return currency(
 		team.doc?.billing_info?.micro_debit_charge_amount,
-		team.doc?.currency
+		team.doc?.currency,
 	);
 });
 </script>

dashboard/src2/components/billing/PrepaidCreditsForm.vue
@@ -34,14 +34,16 @@
 			</FormControl>
 			<FormControl
 				v-if="team.doc.country === 'Kenya' && paymentGateway === 'M-Pesa'"
-				:label="`Amount in KES (Exchange Rate: ${Math.round(exchangeRate)} against 1 USD)`"
+				:label="`Amount in KES (Exchange Rate: ${Math.round(
+					exchangeRate,
+				)} against 1 USD)`"
 				disabled
 				:modelValue="amountInKES"
 				name="amount_in_kes"
 				type="number"
 			>
 				<template #prefix>
-					<div class="grid w-4 place-items-center text-sm text-gray-700 -ml-1">
+					<div class="-ml-1 grid w-4 place-items-center text-sm text-gray-700">
 						{{ 'Ksh' }}
 					</div>
 				</template>
@@ -128,6 +130,9 @@ import { ref, computed, inject, watch, onMounted } from 'vue';
 const emit = defineEmits(['success']);
 
 const team = inject('team');
+const props = defineProps({
+	minimumAmount: Number,
+});
 
 const totalUnpaidAmount = createResource({
 	url: 'press.api.billing.total_unpaid_amount',
@@ -136,6 +141,7 @@ const totalUnpaidAmount = createResource({
 });
 
 const minimumAmount = computed(() => {
+	if (props.minimumAmount) return props.minimumAmount;
 	if (!team.doc) return 0;
 	const unpaidAmount = totalUnpaidAmount.data || 0;
 	const minimumDefault = team.doc?.currency == 'INR' ? 410 : 5;

dashboard/src2/objects/common/index.ts
@@ -5,7 +5,7 @@ import type {
 	ColumnField,
 	DocumentResource,
 	Route,
-	Row
+	Row,
 } from './types';
 import { trialDays } from '../../utils/site';
 import { planTitle } from '../../utils/format';
@@ -23,7 +23,7 @@ export const clusterOptions = [
 	'Singapore',
 	'UAE',
 	'Virginia',
-	'Zurich'
+	'Zurich',
 ];
 
 export function getUpsellBanner(site: DocumentResource, title: string) {
@@ -39,6 +39,7 @@ export function getUpsellBanner(site: DocumentResource, title: string) {
 		title: title,
 		dismissable: true,
 		id: site.name,
+		type: 'gray',
 		button: {
 			label: 'Upgrade Plan',
 			variant: 'outline',
@@ -47,8 +48,8 @@ export function getUpsellBanner(site: DocumentResource, title: string) {
 					() => import('../../components/ManageSitePlansDialog.vue')
 				);
 				renderDialog(h(SitePlansDialog, { site: site.name }));
-			}
-		}
+			},
+		},
 	} satisfies BannerConfig as BannerConfig;
 }
 
@@ -63,13 +64,13 @@ export function getSitesTabColumns(forBenchTab: boolean) {
 			prefix: () => {
 				if (forBenchTab) return;
 				return h('div', { class: 'ml-2 w-3.5 h-3.5' });
-			}
+			},
 		},
 		{
 			label: 'Status',
 			fieldname: 'status',
 			type: 'Badge',
-			width: 0.5
+			width: 0.5,
 		},
 		{
 			label: 'Region',
@@ -80,9 +81,9 @@ export function getSitesTabColumns(forBenchTab: boolean) {
 					return h('img', {
 						src: row.cluster_image,
 						class: 'w-4 h-4',
-						alt: row.cluster_title
+						alt: row.cluster_title,
 					});
-			}
+			},
 		},
 		{
 			label: 'Plan',
@@ -92,8 +93,8 @@ export function getSitesTabColumns(forBenchTab: boolean) {
 					return trialDays(row.trial_end_date);
 				}
 				return planTitle(row);
-			}
-		}
+			},
+		},
 	] satisfies ColumnField[] as ColumnField[];
 }
 
@@ -103,7 +104,7 @@ export function siteTabFilterControls() {
 			type: 'select',
 			label: 'Status',
 			fieldname: 'status',
-			options: ['', 'Active', 'Inactive', 'Suspended', 'Broken']
+			options: ['', 'Active', 'Inactive', 'Suspended', 'Broken'],
 		},
 		{
 			type: 'select',
@@ -120,15 +121,15 @@ export function siteTabFilterControls() {
 				'Singapore',
 				'UAE',
 				'Virginia',
-				'Zurich'
-			]
-		}
+				'Zurich',
+			],
+		},
 	];
 }
 
 export function sitesTabRoute(r: Row) {
 	return {
 		name: 'Site Detail',
-		params: { name: r.name }
+		params: { name: r.name },
 	} satisfies Route as Route;
 }

commit 3c753a5fa250258751f89fc756eeaaa868f8ce36

fix(plan-cards): show label if provided
regression from 3f04819468dbd01bac3ced201cd4bfe7fc7dda42

dashboard/src2/components/PlansCards.vue
@@ -24,8 +24,12 @@
 					<div class="flex items-center justify-between">
 						<div class="flex w-full items-center text-lg">
 							<span class="truncate font-medium text-gray-900">
-								{{ $format.planTitle(plan)
-								}}<span v-if="plan.price_inr" class="text-gray-700">/mo</span>
+								<!-- Needed for app plan selector -->
+								<template v-if="plan.label">{{ plan.label }}</template>
+								<template v-else>
+									{{ $format.planTitle(plan) }}
+									<span v-if="plan.price_inr" class="text-gray-700">/mo</span>
+								</template>
 							</span>
 							<Tooltip text="Support included">
 								<i-lucide-badge-check